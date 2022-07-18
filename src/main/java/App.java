e28c930aace3f0f2fc3e6a7a09f6520dd06ca7ce
GACHIB0SS <der.confederate@gmail.com>
2022-07-18 20:34:54 +0500

    pew pew

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,24 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean2.getMessage());
+
+
+        ApplicationContext applicationContext_cat1 = new AnnotationConfigApplicationContext(AppConfig.class);
+        Cat cat1 = (Cat)applicationContext_cat1.getBean("cat");
+        System.out.println(cat1.getCat());
+
+        Cat cat2 = (Cat)applicationContext_cat1.getBean("cat");
+        System.out.println(cat2.getCat());
+
+
+        System.out.println(bean==bean2);
+        System.out.println(cat1==cat2);
+
+
     }
 }
